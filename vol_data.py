2857c52dfc6ced9ad67220dba0fb4b0725cd516d
Ricky Fleming <raflemin@gmail.com>
2024-08-14 16:54:51 -0700

    convert date to datetime year first

--- vol_data.py
+++ vol_data.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-vix_hist = pd.read_csv('https://cdn.cboe.com/api/global/us_indices/daily_prices/VIX_History.csv')
+vix_hist = pd.read_csv('https://cdn.cboe.com/api/global/us_indices/daily_prices/VIX_History.csv', index_col=False)
 
 # change column names to lowercase
 
 vix_hist.columns = [name.lower() for name in vix_hist.columns]
 
-# change column datatypes
+# change 'date' to datetime
 
+vix_hist['date'] = pd.to_datetime(vix_hist['date'], yearfirst=True)
