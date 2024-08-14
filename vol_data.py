a7fc9aa3711927877cb8ee9d67536fb5b1115cd7
Ricky Fleming <raflemin@gmail.com>
2024-08-14 16:18:01 -0700

    change column names to lowercase

--- vol_data.py
+++ vol_data.py
@@ -1,3 +1,10 @@
 import pandas as pd
 
 vix_hist = pd.read_csv('https://cdn.cboe.com/api/global/us_indices/daily_prices/VIX_History.csv')
+
+# change column names to lowercase
+
+vix_hist.columns = [name.lower() for name in vix_hist.columns]
+
+# change column datatypes
+
